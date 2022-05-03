455b773e2f979a1a37fcefe2a288a26a19d7f826
Jirka Kosek <jirka@kosek.cz>
2022-05-03 15:55:12 +0200

    Basic load/save of PDF file

--- src/main/java/cz/isdoc/pdf/Create.java
+++ src/main/java/cz/isdoc/pdf/Create.java
@@ -1,9 +1,34 @@
 package cz.isdoc.pdf;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Create {
 
     public static void main(String args[]) {
         System.out.println("ISDOC.PDF Creator");
+
+        String pdfFilename = "examples/input/ABRAGen_FV-1_2021-a1.pdf";
+        String isdocFilename = "examples/input/ABRAGx_ISDOC_FV-1-2021.isdoc";
+        String outputFilename = "examples/output/test.pdf";
+
+        PDDocument doc;
+
+        try {
+            System.out.println("Loading PDF file " + pdfFilename + "...");
+            doc = PDDocument.load(new File(pdfFilename));
+            //System.out.println("Pages: " + doc.getNumberOfPages());
+
+
+            System.out.println("Writing merged file " + outputFilename + "...");
+            doc.save(new File(outputFilename));
+            System.out.println("Done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
