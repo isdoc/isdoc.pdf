e08736dd7cadf9b4824f1a197c1b7abb9a1bb650
Jirka Kosek <jirka@kosek.cz>
2022-07-20 16:51:19 +0200

    Updated code to handle CLI parameters and to produce single JAR

--- src/main/java/cz/isdoc/pdf/Create.java
+++ src/main/java/cz/isdoc/pdf/Create.java
@@ -21,89 +21,93 @@ public class Create {
     public static void main(String args[]) {
         System.out.println("ISDOC.PDF Creator");
 
-        String pdfFilename = "examples/input/ABRAGen_FV-1_2021-a1.pdf";
-        String isdocFilename = "examples/input/ABRAGx_ISDOC_FV-1-2021.isdoc";
-        String outputFilename = "examples/output/test.pdf";
-
-        try {
-            PDDocument doc;
-
-            System.out.println("Loading PDF file " + pdfFilename + "...");
-            doc = PDDocument.load(new File(pdfFilename));
-
-            byte[] data = Files.readAllBytes(Paths.get(isdocFilename));
-
-            PDComplexFileSpecification fs = new PDComplexFileSpecification();
-            fs.setFile(isdocFilename);
-
-            COSDictionary dict = fs.getCOSObject();
-            dict.setName("AFRelationship", "Alternative");
-            dict.setString("UF", "invoice.isdoc");
-            //dict.setString("Desc", description);
-
-            ByteArrayInputStream fakeFile = new ByteArrayInputStream(data);
-            PDEmbeddedFile ef = new PDEmbeddedFile(doc, fakeFile);
-            //		ef.addCompression();
-            ef.setSubtype("text/xml");
-            ef.setSize(data.length);
-            ef.setCreationDate(new GregorianCalendar());
-
-            ef.setModDate(GregorianCalendar.getInstance());
-
-            fs.setEmbeddedFile(ef);
-
-            // In addition make sure the embedded file is set under /UF
-            dict = fs.getCOSObject();
-            COSDictionary efDict = (COSDictionary) dict.getDictionaryObject(COSName.EF);
-            COSBase lowerLevelFile = efDict.getItem(COSName.F);
-            efDict.setItem(COSName.UF, lowerLevelFile);
-
-            // now add the entry to the embedded file tree and set in the document.
-            PDDocumentNameDictionary names = new PDDocumentNameDictionary(doc.getDocumentCatalog());
-            PDEmbeddedFilesNameTreeNode efTree = names.getEmbeddedFiles();
-            if (efTree == null) {
-                efTree = new PDEmbeddedFilesNameTreeNode();
-            }
-
-            Map<String, PDComplexFileSpecification> namesMap = new HashMap<>();
-
-            Map<String, PDComplexFileSpecification> oldNamesMap = efTree.getNames();
-            if (oldNamesMap != null) {
-                for (String key : oldNamesMap.keySet()) {
-                    namesMap.put(key, oldNamesMap.get(key));
-                }
-            }
-            namesMap.put("invoice.isdoc", fs);
-            efTree.setNames(namesMap);
-
-            names.setEmbeddedFiles(efTree);
-            doc.getDocumentCatalog().setNames(names);
-
-            // AF entry (Array) in catalog with the FileSpec
-            COSBase AFEntry = (COSBase) doc.getDocumentCatalog().getCOSObject().getItem("AF");
-            if ((AFEntry == null)) {
-                COSArray cosArray = new COSArray();
-                cosArray.add(fs);
-                doc.getDocumentCatalog().getCOSObject().setItem("AF", cosArray);
-            } else if (AFEntry instanceof COSArray) {
-                COSArray cosArray = (COSArray) AFEntry;
-                cosArray.add(fs);
-                doc.getDocumentCatalog().getCOSObject().setItem("AF", cosArray);
-            } else if ((AFEntry instanceof COSObject) &&
-                    ((COSObject) AFEntry).getObject() instanceof COSArray) {
-                COSArray cosArray = (COSArray) ((COSObject) AFEntry).getObject();
-                cosArray.add(fs);
-            } else {
-                throw new IOException("Unexpected object type for PDFDocument/Catalog/COSDictionary/Item(AF)");
-            }
-
-            System.out.println("Writing merged file " + outputFilename + "...");
-            doc.save(new File(outputFilename));
-            System.out.println("Done");
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (args.length != 3) {
+            System.out.println("Usage: java -jar isdoc-pdf.jar <input.pdf> <input.isdoc> <merged.pdf>");
         }
+        else {
+            String pdfFilename = args[0];
+            String isdocFilename = args[1];
+            String outputFilename = args[2];
 
+            try {
+                PDDocument doc;
+
+                System.out.println("Loading PDF file " + pdfFilename + "...");
+                doc = PDDocument.load(new File(pdfFilename));
+
+                byte[] data = Files.readAllBytes(Paths.get(isdocFilename));
+
+                PDComplexFileSpecification fs = new PDComplexFileSpecification();
+                fs.setFile(isdocFilename);
+
+                COSDictionary dict = fs.getCOSObject();
+                dict.setName("AFRelationship", "Alternative");
+                dict.setString("UF", "invoice.isdoc");
+                //dict.setString("Desc", description);
+
+                ByteArrayInputStream fakeFile = new ByteArrayInputStream(data);
+                PDEmbeddedFile ef = new PDEmbeddedFile(doc, fakeFile);
+                //		ef.addCompression();
+                ef.setSubtype("text/xml");
+                ef.setSize(data.length);
+                ef.setCreationDate(new GregorianCalendar());
+
+                ef.setModDate(GregorianCalendar.getInstance());
+
+                fs.setEmbeddedFile(ef);
+
+                // In addition make sure the embedded file is set under /UF
+                dict = fs.getCOSObject();
+                COSDictionary efDict = (COSDictionary) dict.getDictionaryObject(COSName.EF);
+                COSBase lowerLevelFile = efDict.getItem(COSName.F);
+                efDict.setItem(COSName.UF, lowerLevelFile);
+
+                // now add the entry to the embedded file tree and set in the document.
+                PDDocumentNameDictionary names = new PDDocumentNameDictionary(doc.getDocumentCatalog());
+                PDEmbeddedFilesNameTreeNode efTree = names.getEmbeddedFiles();
+                if (efTree == null) {
+                    efTree = new PDEmbeddedFilesNameTreeNode();
+                }
+
+                Map<String, PDComplexFileSpecification> namesMap = new HashMap<>();
+
+                Map<String, PDComplexFileSpecification> oldNamesMap = efTree.getNames();
+                if (oldNamesMap != null) {
+                    for (String key : oldNamesMap.keySet()) {
+                        namesMap.put(key, oldNamesMap.get(key));
+                    }
+                }
+                namesMap.put("invoice.isdoc", fs);
+                efTree.setNames(namesMap);
+
+                names.setEmbeddedFiles(efTree);
+                doc.getDocumentCatalog().setNames(names);
+
+                // AF entry (Array) in catalog with the FileSpec
+                COSBase AFEntry = (COSBase) doc.getDocumentCatalog().getCOSObject().getItem("AF");
+                if ((AFEntry == null)) {
+                    COSArray cosArray = new COSArray();
+                    cosArray.add(fs);
+                    doc.getDocumentCatalog().getCOSObject().setItem("AF", cosArray);
+                } else if (AFEntry instanceof COSArray) {
+                    COSArray cosArray = (COSArray) AFEntry;
+                    cosArray.add(fs);
+                    doc.getDocumentCatalog().getCOSObject().setItem("AF", cosArray);
+                } else if ((AFEntry instanceof COSObject) &&
+                        ((COSObject) AFEntry).getObject() instanceof COSArray) {
+                    COSArray cosArray = (COSArray) ((COSObject) AFEntry).getObject();
+                    cosArray.add(fs);
+                } else {
+                    throw new IOException("Unexpected object type for PDFDocument/Catalog/COSDictionary/Item(AF)");
+                }
+
+                System.out.println("Writing merged file " + outputFilename + "...");
+                doc.save(new File(outputFilename));
+                System.out.println("Done");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
